4907fcced4ed8c96f1293f042c9fe120114ff02d
lashwinth <ls3458@drexel.edu>
2023-10-22 18:20:25 -0400

    93% with comment and docstring

--- src/tic_tac_toe/tic_tac_toe.py
+++ src/tic_tac_toe/tic_tac_toe.py
@@ -1,65 +1,157 @@
 class TicTacToe:
     def __init__(self):
+        "Initialize the Tic-Tac-Toe game board as a 3x3 grid with empty spaces."
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
+
+        # Set the starting player to 'X'. 
+        # 'X' usually starts in the standard game of Tic-Tac-Toe.
         self.current_player = 'X'
 
     def print_board(self):
-        """Print the current game board."""
+        """
+        Print the current game board.
+
+        This method is responsible for displaying the current state of the Tic-Tac-Toe board.
+        It prints the board's grid structure with 'X', 'O', and empty spaces (' ') to represent
+        the moves made by the players.
+
+        Example output:
+        | X | O | X
+        ---------
+        | O | X | O
+        ---------
+        | O | X |  
+
+        """
         for row in self.board:
-            print(" | ".join(row))
-            print("-" * 9)
+            print(" | ".join(row)) # Print a row of cells with vertical bars between them
+            print("-" * 9)         # Print a horizontal line to separate rows
 
     def make_move(self, row, col):
-        """Make a move on the board."""
+        """
+        Make a move on the board.
+        This method allows a player to make a move on the Tic-Tac-Toe board by specifying
+        the row and column where they want to place their symbol ('X' or 'O'). If the
+        specified cell is empty (' '), the move is valid and the player's symbol is placed
+        in that cell. If the cell is already occupied, it raises a ValueError indicating an
+        invalid move.
+
+        Parameters:
+        - row (int): The row where the player wants to place their symbol (0-based index).
+        - col (int): The column where the player wants to place their symbol (0-based index).
+    
+        Raises:
+        - ValueError: If the specified cell is already occupied by a player's symbol.
+    
+        Example usage:
+        make_move(1, 2)  # Places the current player's symbol in the cell at row 1, column 2.
+        make_move(0, 0)  # Raises a ValueError if the cell at row 0, column 0 is already occupied.
+
+    """
         if self.board[row][col] == ' ':
+            # If the specified cell is empty, place the current player's symbol.
             self.board[row][col] = self.current_player
         else:
+            # If the cell is already occupied, raise a ValueError indicating an invalid move.
             raise ValueError("Invalid move. Try again.")
 
     def check_winner(self):
-        """Check if the current player has won."""
+        """
+        Check if the current player has won the game.
+    
+        This method checks the Tic-Tac-Toe board to determine if the current player ('X' or 'O') has
+        achieved a winning combination, either horizontally, vertically, or diagonally. If a winning
+        combination is found, the method returns True; otherwise, it returns False.
+    
+        Returns:
+        - bool: True if the current player has won, False otherwise.
+    
+        Example usage:
+        if check_winner():
+            print(f"Player {self.current_player} wins!")
+
+    """
+        # Check for a horizontal win: all cells in a row contain the current player's symbol.
         for row in self.board:
             if all(cell == self.current_player for cell in row):
                 return True
 
+        # Check for a vertical win: all cells in a column contain the current player's symbol.
         for col in range(3):
             if all(self.board[row][col] == self.current_player for row in range(3)):
                 return True
-
+        
+        # Check for diagonal wins: both diagonals have the current player's symbol.
         if all(self.board[i][i] == self.current_player for i in range(3)) or all(
                 self.board[i][2 - i] == self.current_player for i in range(3)):
             return True
 
+         # If no winning combination is found, return False.
         return False
 
+
     def is_board_full(self):
-        """Check if the board is full (a draw)."""
+        """
+        Check if the Tic-Tac-Toe board is full, indicating a draw.
+    
+        This method examines the Tic-Tac-Toe board to determine if it is completely filled with player symbols
+        ('X' and 'O') or if there are no more empty spaces (' '). If the board is full, it means the game
+        has ended in a draw.
+    
+        Returns:
+        - bool: True if the board is full (a draw), False otherwise.
+    
+        Example usage:
+        if is_board_full():
+            print("It's a draw!")
+    
+    """
+        # Use a list comprehension to check if there are no empty spaces (' ') in the entire board.
+        # If all cells are filled, it means the board is full, and the game is a draw.
         return all(cell != ' ' for row in self.board for cell in row)
 
     def play_game(self):
-        """Play the Tic-Tac-Toe game."""
+        """
+        Play the Tic-Tac-Toe game.
+    
+        This method is the core of the Tic-Tac-Toe game. It allows two players to take turns making moves,
+        checks for a winner, and handles game termination conditions such as a win or a draw.
+    
+        The game continues until a player wins or the board is full, resulting in a draw.
+    
+        Example usage:
+        game = TicTacToe()
+        game.play_game()
+    
+    """
         while True:
-            self.print_board()
+            self.print_board() # Display the current state of the game board
             try:
+                # Prompt the current player to enter their move as 'row' and 'column'.
                 row, col = map(int, input(f"Player {self.current_player}, enter your move (row and column): ").split())
-                self.make_move(row, col)
+                self.make_move(row, col) # Attempt to make the move
             except (ValueError, IndexError):
                 print("Invalid input. Try again.")
                 continue
 
+            # Check if the current player has won
             if self.check_winner():
                 self.print_board()
                 print(f"Player {self.current_player} wins!")
                 break
 
+            # Check if the board is full (a draw)
             if self.is_board_full():
                 self.print_board()
                 print("It's a draw!")
                 break
 
+            # Switch to the other player for the next turn
             self.current_player = 'O' if self.current_player == 'X' else 'X'
 
 
 if __name__ == "__main__":
-    game = TicTacToe()
-    game.play_game()
+    # This block of code is executed only if this Python script is run as the main program.
+
+    game = TicTacToe() # Create an instance of the TicTacToe class to start a new game.
+    game.play_game()   # Call the play_game method to begin the Tic-Tac-Toe game.
--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -4,40 +4,149 @@ import sys
 from tic_tac_toe.tic_tac_toe import TicTacToe
 
 class TestTicTacToe(unittest.TestCase):
+    """
+    A unit test class for the TicTacToe game.
+    This class contains test cases to verify the functionality of the TicTacToe class.
+
+    """
     def setUp(self):
-        self.game = TicTacToe()
+        """
+        Set up test environment for each test case.
+
+        This method is called before each individual test case to prepare the testing environment.
+        It creates an instance of the TicTacToe class, which can be used for testing.
+
+        """
+        self.game = TicTacToe()  # Create an instance of the TicTacToe class for testing
 
     def test_print_board(self):
+        """
+        Test the 'print_board' method of the TicTacToe class.
+    
+        This test case captures the printed output of the 'print_board' method and compares it with the
+        expected output to verify that the method correctly displays the Tic-Tac-Toe game board.
+    
+    """
+        # Capture the standard output (print) to a StringIO buffer for testing.
         captured_output = StringIO()
         sys.stdout = captured_output
+
+        # Call the 'print_board' method to print the Tic-Tac-Toe game board.
         self.game.print_board()
+
+        # Restore the original standard output.
         sys.stdout = sys.__stdout__
-        expected_output = " | | \n---------\n | | \n---------\n | | \n---------\n"
+
+        # Define the expected output for an empty game board.
+        expected_output = "  |   |  \n---------\n  |   |  \n---------\n  |   |  \n---------\n"
+        
+         # Check if the captured output matches the expected output.
         self.assertEqual(captured_output.getvalue(), expected_output)
 
     def test_make_move(self):
+        """
+        Test the 'make_move' method of the TicTacToe class.
+    
+        This test case checks the behavior of the 'make_move' method by making valid and invalid moves and
+        verifying that the board is updated correctly and that it raises an exception for an invalid move.
+
+    """
+        # Make a valid move for 'X' at row 0, column 0.
         self.game.make_move(0, 0)
+
+        # Check if the board was updated correctly, and the specified cell contains 'X'.
         self.assertEqual(self.game.board[0][0], 'X')
 
+        # Attempt to make another move at the same position, which should be invalid.
+        # Verify that the 'make_move' method raises a ValueError.
         with self.assertRaises(ValueError):
             self.game.make_move(0, 0)
 
+
     def test_check_winner(self):
+        """
+        Test the 'check_winner' method of the TicTacToe class.
+    
+        This test case checks the 'check_winner' method for various winning scenarios.
+        It sets up different board configurations where 'X' or 'O' wins and verifies that the method returns True.
+
+    """
+        # Test case 1: 'X' wins horizontally in the top row.
         self.game.board = [['X', 'X', 'X'], [' ', ' ', 'O'], ['O', ' ', ' ']]
         self.assertTrue(self.game.check_winner())
 
+        # Test case 2: 'X' wins diagonally from top-left to bottom-right.
         self.game.board = [['X', 'O', 'O'], [' ', 'X', 'O'], ['O', ' ', 'X']]
         self.assertTrue(self.game.check_winner())
 
+        # Test case 3: 'O' wins vertically in the left column.
         self.game.board = [['O', 'O', 'X'], ['O', 'X', ' '], ['X', ' ', 'O']]
         self.assertTrue(self.game.check_winner())
 
+
+
     def test_is_board_full(self):
+        """
+        Test the 'is_board_full' method of the TicTacToe class.
+    
+        This test case checks the 'is_board_full' method for two different board configurations:
+        one where the board is full (a draw) and another where there are still empty spaces.
+        It verifies that the method returns True for a full board and False otherwise.
+    """
+        # Test case 1: A full board (a draw) with no empty spaces.
         self.game.board = [['X', 'O', 'X'], ['O', 'X', 'O'], ['O', 'X', 'X']]
         self.assertTrue(self.game.is_board_full())
-
+     
+        # Test case 2: A partially filled board with some empty spaces.
         self.game.board = [['X', 'O', 'X'], ['O', 'X', 'O'], ['O', 'X', ' ']]
         self.assertFalse(self.game.is_board_full())
 
+
+
+    def test_play_game(self):
+        """
+        Test the 'play_game' method of the TicTacToe class.
+        This test case simulates a game where 'X' wins and verifies that the game outcome matches the expected output.
+    
+    """
+        # Simulate a game where 'X' wins
+        user_inputs = ['0 0', '0 1', '1 0', '1 1', '2 0']
+
+        # Define the expected output to match the game simulation
+        expected_output = (
+            "  |   |  \n---------\n  |   |  \n---------\n  |   |  \n---------\n"
+            "Player X, enter your move (row and column): "
+            "X |   |  \n---------\n  |   |  \n---------\n  |   |  \n---------\n"
+            "Player O, enter your move (row and column): "
+            "X | O |  \n---------\n  |   |  \n---------\n  |   |  \n---------\n"
+            "Player X, enter your move (row and column): "
+            "X | O |  \n---------\nX |   |  \n---------\n  |   |  \n---------\n"
+            "Player O, enter your move (row and column): "
+            "X | O |  \n---------\nX | O |  \n---------\n  |   |  \n---------\n"
+            "Player X, enter your move (row and column): "
+            "X | O |  \n---------\nX | O |  \n---------\nX |   |  \n---------\n"
+            "Player X wins!\n"
+        )
+
+        # Capture the standard output and input to simulate the game
+        captured_output = StringIO()
+        captured_input = StringIO("\n".join(user_inputs)) # Join the user inputs with newlines
+        sys.stdout = captured_output
+        sys.stdin = captured_input
+
+        # Call the 'play_game' method to simulate the game
+        self.game.play_game()
+        self.maxDiff = None
+
+        # Restore the original standard output and input
+        sys.stdout = sys.__stdout__
+        sys.stdin = sys.__stdin__
+
+        # Verify that the captured output matches the expected output
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+
 if __name__ == "__main__":
+    # This block of code is executed only if this Python script is run as the main program.
+    # Run the unit tests defined in this script using the unittest module.
     unittest.main()
